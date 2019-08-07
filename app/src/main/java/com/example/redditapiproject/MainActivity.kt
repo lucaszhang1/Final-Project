f4cbd89fef15bc20f40fbd1df60616825dc273a6
foobles <buzdav2@gmail.com>
2019-08-07 14:07:08 -0500

    Attached REST code to recyclerView

--- app/src/main/java/com/example/redditapiproject/MainActivity.kt
+++ app/src/main/java/com/example/redditapiproject/MainActivity.kt
@@ -14,15 +14,7 @@ import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 
-private fun <K, V> MutableMap<K, V>.modifyOrDefault(key: K, default: V, mod: (V) -> V) {
-    val cur = this[key]
-    this[key] = when (cur) {
-        null -> default
-        else -> mod(cur)
-    }
-}
 
-data class SubInfoDebug(val name: String, val hits: Int, val userCount: Int )
 
 
 class MainActivity : AppCompatActivity() {
@@ -33,11 +25,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GlobalScope.launch {
-            for ((sub, hits, count) in getTopSubHits()) {
-                Log.d("SUB HITS", "$sub: \t\t\t$hits - $count")
-            }
-        }
 
         navController = findNavController(R.id.navHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
@@ -47,69 +34,4 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp() || super.onNavigateUp()
     }
 
-    private fun getTopSubHits(): Channel<SubInfoDebug> {
-        val chan = Channel<SubInfoDebug>()
-        APICommunitySubmissionRestClient.instance.getSubmissionList(object : RetrofitEventListener {
-            override fun onSuccess(call: Call<*>?, response: Any) {
-                val users = mutableSetOf<String>()
-
-                if (response is SubmissionListing) {
-                    Log.d("NETWORK", "Got a subreddit:")
-                    for (x in response.data?.children!!) {
-                        users.add(x.data?.author!!)
-                    }
-
-                    GlobalScope.launch {
-                        val hitMap = mutableMapOf<String, Pair<Int, Int>>()
-
-                        for (user in users) {
-                            for ((subreddit, hits) in getUserHits(user)) {
-                                hitMap.modifyOrDefault(subreddit, Pair(hits, 1)) {
-                                    Pair(it.first + hits, it.second + 1)
-                                }
-                            }
-                        }
-
-                        for ((sub, x) in hitMap) chan.send(SubInfoDebug(sub, x.first, x.second))
-                        chan.close()
-                    }
-                }
-            }
-
-
-            override fun onError(call: Call<*>?, t: Throwable?) {
-                throw t ?: Throwable("Unknown network error!")
-            }
-        })
-        return chan
-    }
-
-    fun getUserHits(user: String): Channel<Pair<String, Int>> {
-        val chan = Channel<Pair<String, Int>>()
-
-        APIUserSubmissionRestClient.instance.getSubmissionList(user, object : RetrofitEventListener {
-            override fun onSuccess(call: Call<*>?, response: Any) {
-                val hitMap = mutableMapOf<String, Int>()
-
-                if (response is SubmissionListing) {
-                    Log.d("NETWORK", "Got a user:")
-                    for (x in response.data?.children!!) {
-                        hitMap.modifyOrDefault(x.data?.subreddit!!, 1) {
-                            it + 1
-                        }
-                    }
-                }
-                GlobalScope.launch {
-                    for (kvp in hitMap) chan.send(kvp.toPair())
-                    chan.close()
-                }
-            }
-
-            override fun onError(call: Call<*>?, t: Throwable?) {
-                throw t ?: Throwable("Unknown network error!")
-            }
-        })
-        return chan
-        
-    }
 }
--- app/src/main/java/com/example/redditapiproject/fragments/Outcome.kt
+++ app/src/main/java/com/example/redditapiproject/fragments/Outcome.kt
@@ -3,17 +3,30 @@ package com.example.redditapiproject.fragments
 
 import android.os.Bundle
 import android.telephony.SubscriptionInfo
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ctdrecviewtutorial.adapter.SubInfoRecycleViewAdapter
 import com.example.redditapiproject.R
 import com.example.redditapiproject.models.SubInfo
+import com.example.redditapiproject.models.SubmissionListing
+import com.example.redditapiproject.network.APICommunitySubmissionRestClient
+import com.example.redditapiproject.network.APIUserSubmissionRestClient
+import com.example.redditapiproject.network.RetrofitEventListener
 import kotlinx.android.synthetic.main.fragment_outcome.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import retrofit2.Call
+import com.example.redditapiproject.MainActivity as MainActivity1
 
 /**
  * A simple [Fragment] subclass.
@@ -41,15 +54,104 @@ class Outcome : Fragment() {
             this.adapter = adapter
         }
 
+
+        //adapter.submitList(listOf())
+
         displayTestData(adapter)
 
     }
 
     private fun displayTestData(adapter: SubInfoRecycleViewAdapter) {
-        val testData = listOf(
-            SubInfo("A", 85000, 123),
-            SubInfo("B", 85000, 123),
-            SubInfo("C", 85000, 123))
-        adapter.submitList(testData)
+        val x = GlobalScope.launch {
+            Log.d("RUNBLOCK", "running blocking in dislpayTestData")
+            val data = mutableListOf<SubInfo>()
+                val channel = getTopSubHits()
+                for (s in channel) {
+                    val (sub, hits, count) = s
+                    Log.d("SUB", "Sub found: $sub, $hits, $count")
+                    data.add(s)
+                }
+            activity?.runOnUiThread {
+                adapter.submitList(data)
+            }
+        }
+
+
+    }
+
+    private fun getTopSubHits(): Channel<SubInfo> {
+        val chan = Channel<SubInfo>()
+        APICommunitySubmissionRestClient.instance.getSubmissionList(object : RetrofitEventListener {
+            override fun onSuccess(call: Call<*>?, response: Any) {
+                val users = mutableSetOf<String>()
+
+                if (response is SubmissionListing) {
+                    Log.d("NETWORK", "Got a subreddit:")
+                    for (x in response.data?.children!!) {
+                        users.add(x.data?.author!!)
+                    }
+
+                    GlobalScope.launch {
+                        val hitMap = mutableMapOf<String, Pair<Int, Int>>()
+
+                        for (user in users) {
+                            for ((subreddit, hits) in getUserHits(user)) {
+                                hitMap.modifyOrDefault(subreddit, Pair(hits, 1)) {
+                                    Pair(it.first + hits, it.second + 1)
+                                }
+                            }
+                        }
+
+                        for ((sub, x) in hitMap) chan.send(SubInfo(sub, x.first, x.second))
+                        chan.close()
+                        Log.d("CHAN", "USERHIT CHANNEL CLOSED")
+                    }
+                }
+            }
+
+
+            override fun onError(call: Call<*>?, t: Throwable?) {
+                throw t ?: Throwable("Unknown network error!")
+            }
+        })
+        Log.d("CHAN", "CHAN SENT")
+        return chan
+    }
+
+    fun getUserHits(user: String): Channel<Pair<String, Int>> {
+        val chan = Channel<Pair<String, Int>>()
+
+        APIUserSubmissionRestClient.instance.getSubmissionList(user, object : RetrofitEventListener {
+            override fun onSuccess(call: Call<*>?, response: Any) {
+                val hitMap = mutableMapOf<String, Int>()
+
+                if (response is SubmissionListing) {
+                    Log.d("NETWORK", "Got a user:")
+                    for (x in response.data?.children!!) {
+                        hitMap.modifyOrDefault(x.data?.subreddit!!, 1) {
+                            it + 1
+                        }
+                    }
+                }
+                GlobalScope.launch {
+                    for (kvp in hitMap) chan.send(kvp.toPair())
+                    chan.close()
+                }
+            }
+
+            override fun onError(call: Call<*>?, t: Throwable?) {
+                throw t ?: Throwable("Unknown network error!")
+            }
+        })
+        return chan
+
+    }
+}
+
+private fun <K, V> MutableMap<K, V>.modifyOrDefault(key: K, default: V, mod: (V) -> V) {
+    val cur = this[key]
+    this[key] = when (cur) {
+        null -> default
+        else -> mod(cur)
     }
 }
